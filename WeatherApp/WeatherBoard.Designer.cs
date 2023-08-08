abda312271faf5e2c920eb2404b3a879c3794252
Chathura Gunawardhana <Chathura.LiyanaGunawardhana@bet365.com>
2023-08-08 16:51:55 +0100

    add resources and edit main form

--- WeatherApp/WeatherBoard.Designer.cs
+++ WeatherApp/WeatherBoard.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WeatherBoard));
+            this.SuspendLayout();
+            // 
+            // WeatherBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "WeatherBoard";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "WeatherBoard";
+            this.Text = "Weather App";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
