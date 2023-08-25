feac46222b8c0731f2c009d3c21bf47dcbd162ab
Chathura Gunawardhana <Chathura.LiyanaGunawardhana@bet365.com>
2023-08-25 17:02:29 +0100

    commitr from office - 2023-08-25

---
+++ WeatherApp/Widget.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace WeatherApp
+{
+    public partial class Widget : Form
+    {
+        public Widget()
+        {
+            InitializeComponent();
+        }
+    }
+}
