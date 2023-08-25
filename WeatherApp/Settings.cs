355c48ff6476e172175d145f70790aa607e2b004
Chathura Gunawardhana <Chathura.LiyanaGunawardhana@bet365.com>
2023-08-25 17:02:29 +0100

    commitr from office - 2023-08-25

---
+++ WeatherApp/Settings.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+namespace WeatherApp
+{
+    class Settings
+    {
+        // List of supported cities
+        public static Dictionary<string, int> CITIES_IDS = new Dictionary<string, int>()
+        {
+            { "Calgary", 5913490 },
+            { "Toronto", 6087824 },
+            { "Vancouver", 6090785 },
+            { "Saskatoon", 6141256 },
+            { "Quebec", 6325494 },
+            { "Airdrie", 5882799 },
+            { "Medicine Hat", 6071618},
+            { "Banff", 5892532 },
+            { "California", 4350049 },
+            { "Florida", 3851244 }
+        };
+
+        // API Public Key
+        public static string API_KEY = "49dd73dca58244a685c52418182910";
+
+        // Weather API URL
+        public static string WEATHER_URL = "http://api.apixu.com/v1/forecast.json?key=";
+
+        // Forecast days limit
+        public static int FORECAST_DAYS_LIMIT = 5;
+
+    }
+}
--- WeatherApp/WeatherBoard.cs
+++ WeatherApp/WeatherBoard.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 namespace WeatherApp
 {
     public partial class WeatherBoard : Form
-    { 
+    {
         List<City> cityList;
 
         public static Label LabelSpinner { get; set; }
@@ -20,118 +20,118 @@ namespace WeatherApp
         private bool isDragging = false;
         private Point newPoint = new Point(0, 0);
 
-    public WeatherBoard()
-    {
-        InitializeComponent();
-        FillListCityData();
-        FillCityNamesToCityComboBox();
-        LabelSpinner = labelSpinner;
-    }
-
-    private void FillListCityData()
-    {
-        cityList = new List<City>();
-
-        foreach (var pair in Constant.MapOfCityNameAndID)
+        public WeatherBoard()
         {
-            City city = new City(pair.Key);
-            cityList.Add(city);
-        }
-    }
-
-    private void FillCityNamesToCityComboBox()
-    {
-        List<string> cityNames = new List<string>();
-
-        foreach (var city in cityList)
-        {
-            cityNames.Add(city.Name);
+            InitializeComponent();
+            FillListCityData();
+            FillCityNamesToCityComboBox();
+            LabelSpinner = labelSpinner;
         }
 
-        //cbCityList.DataSource = cityNames;
-    }
-
-    private void btnConfirm_Click(object sender, EventArgs e)
-    {
-        LabelSpinner.Visible = true;
-        // get current selected city obj 
-        string currentSelectedCityName = selectCityTextBox.Text;
-
-        // pass it to API 
-        API apiWeather = new API(currentSelectedCityName, this);
-        //fetch api to get current weather data and store them in CurrentWeatherResponseFromAPI field
-        API.FetchWeatherDataFromAPI(false);
-        selectCityTextBox.Text = "";
-
-    }
-
-    private void WeatherBoard_Load(object sender, EventArgs e)
-    {
-        labelSpinner.Visible = false;
-
-        AutoCompleteStringCollection cityColl = new AutoCompleteStringCollection();
-
-        string[] listCityNames = Helper.GetListOfCityNames();
-
-        foreach (var cityname in listCityNames)
+        private void FillListCityData()
         {
-            cityColl.Add(cityname);
+            cityList = new List<City>();
+
+            foreach (var cityIDpair in Settings.CITIES_IDS)
+            {
+                City city = new City(cityIDpair.Key);
+                cityList.Add(city);
+            }
         }
 
-        selectCityTextBox.AutoCompleteMode = AutoCompleteMode.Suggest;
-        selectCityTextBox.AutoCompleteSource = AutoCompleteSource.CustomSource;
-        selectCityTextBox.AutoCompleteCustomSource = cityColl;
-    }
-
-    private void Refresh_Click(object sender, EventArgs e)
-    {
-        Close();
-    }
-
-    //moves window around based on mouse movement
-    private void panel1_MouseMove(object sender, MouseEventArgs e)
-    {
-        if (isDragging)
+        private void FillCityNamesToCityComboBox()
         {
-            Point p = PointToScreen(e.Location);
-            Location = new Point(p.X - newPoint.X, p.Y - newPoint.Y);
+            List<string> cityNames = new List<string>();
+
+            foreach (var city in cityList)
+            {
+                cityNames.Add(city.Name);
+            }
+
+            //cbCityList.DataSource = cityNames;
         }
-    }
 
-    //when mouse is down on panel
-    private void panel1_MouseDown(object sender, MouseEventArgs e)
-    {
-        isDragging = true;
-        newPoint = new Point(e.X, e.Y);
-
-    }
-
-    //when user lets go of mouse
-    private void panel1_MouseUp(object sender, MouseEventArgs e)
-    {
-        isDragging = false;
-    }
-
-    //minimize
-    private void button1_Click(object sender, EventArgs e)
-    {
-        this.WindowState = FormWindowState.Minimized;
-    }
-
-    private void EnteringCity(object sender, KeyEventArgs e)
-    {
-        if (e.KeyCode == Keys.Enter)
+        private void btnConfirm_Click(object sender, EventArgs e)
         {
-            btnConfirm.PerformClick();
-        }
-    }
+            LabelSpinner.Visible = true;
+            // get current selected city obj 
+            string currentSelectedCityName = selectCityTextBox.Text;
 
-    private void WeatherBoard_FormClosing(object sender, FormClosingEventArgs e)
-    {
-        if (Application.OpenForms.Count == 1)
+            // pass it to API 
+            API apiWeather = new API(currentSelectedCityName, this);
+            //fetch api to get current weather data and store them in CurrentWeatherResponseFromAPI field
+            API.FetchWeatherDataFromAPI(false);
+            selectCityTextBox.Text = "";
+
+        }
+
+        private void WeatherBoard_Load(object sender, EventArgs e)
         {
-            Application.Exit();
-        }
-    }
+            labelSpinner.Visible = false;
 
+            AutoCompleteStringCollection cityColl = new AutoCompleteStringCollection();
+
+            string[] listCityNames = Helper.GetListOfCityNames();
+
+            foreach (var cityname in listCityNames)
+            {
+                cityColl.Add(cityname);
+            }
+
+            selectCityTextBox.AutoCompleteMode = AutoCompleteMode.Suggest;
+            selectCityTextBox.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            selectCityTextBox.AutoCompleteCustomSource = cityColl;
+        }
+
+        private void Refresh_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        //moves window around based on mouse movement
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - newPoint.X, p.Y - newPoint.Y);
+            }
+        }
+
+        //when mouse is down on panel
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDragging = true;
+            newPoint = new Point(e.X, e.Y);
+        }
+
+        //when user lets go of mouse
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+        }
+
+        //minimize
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void EnteringCity(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnConfirm.PerformClick();
+            }
+        }
+
+        private void WeatherBoard_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (Application.OpenForms.Count == 1)
+            {
+                Application.Exit();
+            }
+        }
+
+    }
 }
